ce6b3bc5ad71b934a13f7322662249ee27016942
dga148@utulsa.edu <dga148@utulsa.edu>
2015-08-25 21:32:58 -0500

    Fixed writing to file

--- proj0.c
+++ proj0.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define MAX_LINE_LENGTH 72;
 int main(int argc, char *argv[]) 
 {
 	if(argc == 3) 
 	{
 		FILE *src = fopen(argv[1], "r"); 
-		char line[73] = {0};
+		//lines can only be 72 chars long
+		char line[MAX_LINE_LENGTH] = {0};
 		if(src != NULL)
 		{
 			FILE *dest = fopen(argv[2], "w");
 			if(dest != NULL) 
 			{
+				//determine total file size
 				fseek (src, 0, SEEK_END);
 				int srcLength = ftell (src);
 				rewind (src);
 
 				int linePos = 0;
 				int filePos = 0;
+				int lineNumber = 1;
+				//iterate line by line 
 				while(filePos < srcLength) 
 				{
-					while(linePos < 72 && filePos < srcLength) 
+					//iterate character by character
+					//go to end of line or end of file
+					while(linePos < MAX_LINE_LENGTH && filePos < srcLength) 
 					{
 						char charAtPos;
 						fscanf(src, "%c", &charAtPos);
-						linePos++;
-						filePos++;
 						if(charAtPos == '\n') {
+							linePos++;
+							filePos++;
 							break;
 						}	
+						
 						line[linePos] = charAtPos;
+						linePos++;
+						filePos++;
 					}
-					line[72] = '\0';
-					printf("1:\t%s", line);
+					//write to dest file
+					fprintf(dest, "%d:\t%s\n", lineNumber, line);
 					linePos = 0;
-					memset(line, 0, 72); //zero out array
+					memset(line, 0, MAX_LINE_LENGTH); //zero out array
+					lineNumber++;
 				}
 			}
 			else 
@@ -51,7 +61,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		puts("Invalid number of arguments. Valid syntax is 'proj0 /path/to/source /path/to/destination");
+		puts("Invalid number of arguments. Valid syntax is 'proj0 /path/to/source /path/to/destination'");
 		exit(1);
 	}
 	puts("end of program");
