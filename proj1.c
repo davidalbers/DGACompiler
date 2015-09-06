5d99ca09fafb69e0508b69cc7d155b57988929fa
dga148@utulsa.edu <dga148@utulsa.edu>
2015-09-05 22:06:34 -0500

    Finished tokens

--- proj1.c
+++ proj1.c
@@ -10,10 +10,34 @@
 #define ADDOP 33;
 #define MULOP 34;
 #define ASSIGNOP 35;
+#define LEXERR 99;
+#define INTTOOLONG 2;
+#define UNRECOGNIZEDNUM 3;
+#define DECIMALTOOLONG 4;
+#define REALTOOLONG 5;
+#define LESSTHAN 0;
+#define LESSTHANEQUAL 1;
+#define NOTEQUAL 2;
+#define GREATERTHAN 3;
+#define GREATERTHANEQUAL 4;
+#define EQUAL 5;
+#define ADD 0;
+#define SUBTRACT 1;
+#define OR 2;
+#define MULTIPLY 0;
+#define DIVIDE 1; 
+#define DIV 2;
+#define MOD 3;
+#define AND 4;
+
+union Attribute {
+	char *attrString;
+	int attrInt;
+};
 
 struct token {
 	int tokenName;
-	char *attribute;
+	union Attribute *attribute;
 };
 
 struct FSA {
@@ -40,7 +64,7 @@ int fileLength(FILE *);
 char **readFileLineByLine(FILE *, int);
 void idMachine(char *);
 void whitespaceMachine(char *);
-void getNextToken(char *);
+int getNextToken(char *);
 void relopMachine(char *);
 void realMachine(char *);
 void catchAllMachine(char *);
@@ -85,7 +109,9 @@ void sourceToListing(char *sourcePath, char *listingPath)
 			
 			while(i < numberLines) 
 			{
-				getNextToken(lines[i]);
+				while(getNextToken(lines[i]) == 0) {
+
+				}
 				i++;
 			}
 			i = 0;
@@ -176,8 +202,9 @@ void loadReservedWords(char *pathToReservedWords) {
 	}	
 }
 	
-
-void getNextToken(char *line) {
+//Return 1 at end of line
+//Return 0 if token or error
+int getNextToken(char *line) {
 	fsa.state = 0;
 	while(fsa.f < MAX_LINE_LENGTH) {
 		switch(fsa.state) 
@@ -197,7 +224,7 @@ void getNextToken(char *line) {
 			case 15:
 				newLineMachine(line);
 				if(fsa.state == 0)
-					return;
+					return 1;
 				break;
 			case 16:
 				addopMachine(line);
@@ -209,13 +236,20 @@ void getNextToken(char *line) {
 				assignopMachine(line);
 				break;
 			case 101:
-				printf("got token name: '%d' attribute: '%s'\n",fsa.currToken->tokenName,fsa.currToken->attribute);
-				return;
+				//seg fault if try to read attrString and there wasn't one
+				//only read applicable type for token
+				//theres probably a better way than >= 32
+				if(fsa.currToken->tokenName >= 32)
+					printf("got token name: '%d' attribute:  '%d'\n",fsa.currToken->tokenName,fsa.currToken->attribute->attrInt);
+				else 
+					printf("got token name: '%d' attribute:  '%s'\n",fsa.currToken->tokenName,fsa.currToken->attribute->attrString);
+				return 0;
 			default:
 				catchAllMachine(line);
 				break;
 		}
 	}
+	return 0;
 }
 
 void whitespaceMachine(char *line) {
@@ -240,6 +274,7 @@ void idMachine(char *line) {
 	int idLength = 0;
 	while(fsa.f < MAX_LINE_LENGTH && idLength < 10) {
 		char c = line[fsa.f];
+		// printf("idMachine got '%c'\n",c);
 		fsa.f++;
 		switch(fsa.state) 
 		{
@@ -267,15 +302,19 @@ void idMachine(char *line) {
 					if(checkInResWordTable(id)) {
 						printf("id is a reserved word '%s'\n", id);
 						struct token idToken;
+						union Attribute attr;
+						attr.attrString = id;
 						idToken.tokenName = RESERVED;
-						idToken.attribute = id;
+						idToken.attribute = &attr;
 						fsa.currToken = &idToken;
 						return;
 					}
 					addToSymbolTable(id);
 					struct token idToken;
+					union Attribute attr;
+					attr.attrString = id;
 					idToken.tokenName = ID;
-					idToken.attribute = id;
+					idToken.attribute = &attr;
 					fsa.currToken = &idToken;
 					return;
 				}
@@ -285,8 +324,11 @@ void idMachine(char *line) {
 }
 
 void relopMachine(char* line) {
+	struct token relopToken;
+	union Attribute attr;
 	while(fsa.f < MAX_LINE_LENGTH) {
 		char relop = line[fsa.f];
+		// printf("relopMachine got '%c'\n",relop);
 		fsa.f++;
 		switch(fsa.state) 
 		{
@@ -296,9 +338,9 @@ void relopMachine(char* line) {
 				else if(relop == '>') 
 					fsa.state = 6;
 				else if(relop == '=') {
-					struct token relopToken;
 					relopToken.tokenName = RELOP;
-					relopToken.attribute = relop;
+					attr.attrInt = EQUAL;
+					relopToken.attribute = &attr;
 					fsa.currToken = &relopToken;
 					fsa.state = 101;
 					return;
@@ -311,21 +353,30 @@ void relopMachine(char* line) {
 				break;
 			case 5:
 				//any character here results in a relop
-				
-				if(relop != '=' && relop != '>') 
-					fsa.f--; //'<' + another, non relop, character so just <
-				struct token relopToken;
 				relopToken.tokenName = RELOP;
-				relopToken.attribute = relop;
+				if(relop == '=') {
+					attr.attrInt = LESSTHANEQUAL;
+				}
+				else if(relop == '>') {
+					attr.attrInt = NOTEQUAL;
+				}
+				else {
+					fsa.f--; //'<' + another, non relop, character, so just <
+					attr.attrInt = LESSTHAN;
+				}
+				relopToken.attribute = &attr;
 				fsa.currToken = &relopToken;
 				fsa.state = 101;
 				return;
 			case 6:
-				if(relop != '=') 
-					fsa.f--;//'>'+ another, non relop, character so just >
-				struct token relopToken;
 				relopToken.tokenName = RELOP;
-				relopToken.attribute = relop;
+				if(relop == '=') 
+					attr.attrInt = GREATERTHANEQUAL;
+				if(relop != '=') {
+					attr.attrInt = GREATERTHAN;
+					fsa.f--;//'>'+ another, non relop, character, so just >
+				}
+				relopToken.attribute = &attr;
 				fsa.currToken = &relopToken;
 				fsa.state = 101;
 				return;	
@@ -338,16 +389,21 @@ void realMachine(char *line) {
 	char *d = malloc(sizeof(char) * 10);
 	char *yy = malloc(sizeof(char) * 5);
 	char *zz = malloc(sizeof(char) * 2);
+	char *num = malloc(sizeof(char) * 17);
 	int dCount = 0;
 	int yyCount = 0;
 	int zzCount = 0;
-	int lengthError = 0;
+	int intLengthError = 0;
+	int decimalLengthError = 0;
+	int realLengthError = 0;
 	//assumed to be positive unless said otherwise
 	int negative = 0; 
 	while(fsa.f < MAX_LINE_LENGTH) {
 		char c = line[fsa.f];
+		// printf("realMachine got '%c'\n",c);
 		fsa.f++;
 		// printf("%dreal machine read %c\n",fsa.state, c);
+		//todo add leading zero check
 		switch(fsa.state) 
 		{
 			case 7:
@@ -365,8 +421,7 @@ void realMachine(char *line) {
 			case 8:
 				if(isdigit(c)) {
 					if(dCount >= 10) {
-						
-						lengthError = 1;
+						intLengthError = 1;
 					}
 					d[dCount] = c;
 					dCount++;
@@ -378,15 +433,25 @@ void realMachine(char *line) {
 					fsa.state = 11;
 				}
 				else {
-					if(lengthError) {
-						puts("real machine got length error");
+					if(intLengthError) {
+						struct token errToken;
+						errToken.tokenName = LEXERR;
+						union Attribute attr;
+						attr.attrInt = INTTOOLONG;
+						errToken.attribute = &attr;
+						fsa.currToken = &errToken;
+						fsa.state = 101;
 					}
 					else {
-						printf("real machine got int %s\n", d);
-						
+						struct token numToken;
+						numToken.tokenName = NUM;
+						union Attribute attr;
+						attr.attrString = d;
+						numToken.attribute = &attr;
+						fsa.currToken = &numToken;
+						fsa.state = 101;
 					}
 					fsa.f--;
-					fsa.state = 0;
 					return;
 				}
 				break;
@@ -398,15 +463,20 @@ void realMachine(char *line) {
 				}
 				else {
 					fsa.f--;
-					printf("real machine got error2 %c\n", c);
-					fsa.state = 100;
+					struct token errToken;
+					errToken.tokenName = LEXERR;
+					union Attribute attr;
+					attr.attrInt = UNRECOGNIZEDNUM;
+					errToken.attribute = &attr;
+					fsa.currToken = &errToken;
+					fsa.state = 101;
 					return;
 				}
 				break;
 			case 10:
 				if(isdigit(c)) {
 					if(yyCount >= 5) {
-						lengthError = 1;
+						decimalLengthError = 1;
 					}
 					else {
 						yy[yyCount] = c;
@@ -417,15 +487,38 @@ void realMachine(char *line) {
 					fsa.state = 11;
 				}
 				else {
-					if(lengthError) {
-						puts("real machine got length error");
+					if(intLengthError) {
+						struct token errToken;
+						errToken.tokenName = LEXERR;
+						union Attribute attr;
+						attr.attrInt = INTTOOLONG;
+						errToken.attribute = &attr;
+						fsa.currToken = &errToken;
+						fsa.state = 101;
+					}
+					else if(decimalLengthError) {
+						struct token errToken;
+						errToken.tokenName = LEXERR;
+						union Attribute attr;
+						attr.attrInt = DECIMALTOOLONG;
+						errToken.attribute = &attr;
+						fsa.currToken = &errToken;
+						fsa.state = 101;
 					}
 					else {
-						printf("real machine got decimal %s.%s\n", d,yy);
-						
+						struct token numToken;
+						numToken.tokenName = NUM;
+						union Attribute attr;
+						strcpy(num,d);
+						strcat(num,".");
+						strcat(num,yy);
+						attr.attrString = num;
+						numToken.attribute = &attr;
+						fsa.currToken = &numToken;
+						fsa.state = 101;
+				
 					}
 					fsa.f--;
-					fsa.state = 0;
 					return;
 				}
 				break;
@@ -442,8 +535,13 @@ void realMachine(char *line) {
 
 				}
 				else {
-					printf("real machine got error3 %c\n", c);
-					fsa.state = 100;
+					struct token errToken;
+					errToken.tokenName = LEXERR;
+					union Attribute attr;
+					attr.attrInt = UNRECOGNIZEDNUM;
+					errToken.attribute = &attr;
+					fsa.currToken = &errToken;
+					fsa.state = 101;
 					fsa.f--;
 					return;
 				}
@@ -455,30 +553,95 @@ void realMachine(char *line) {
 					zzCount++;
 				}
 				else {
-					if(lengthError) {
-						puts("real machine got length error");
+					if(intLengthError) {
+						struct token errToken;
+						errToken.tokenName = LEXERR;
+						union Attribute attr;
+						attr.attrInt = INTTOOLONG;
+						errToken.attribute = &attr;
+						fsa.currToken = &errToken;
+						fsa.state = 101;
+		
+					}
+					else if(decimalLengthError) {
+						struct token errToken;
+						errToken.tokenName = LEXERR;
+						union Attribute attr;
+						attr.attrInt = DECIMALTOOLONG;
+						errToken.attribute = &attr;
+						fsa.currToken = &errToken;
+						fsa.state = 101;
+			
 					}
 					else {
-						printf("real machine got real %s.%se%s\n",d,yy,zz);
+						struct token numToken;
+						numToken.tokenName = NUM;
+						union Attribute attr;
+						strcpy(num,d);
+						strcat(num,".");
+						strcat(num,yy);
+						strcat(num,"E");
+						strcat(num,zz);
+						attr.attrString = num;
+						numToken.attribute = &attr;
+						fsa.currToken = &numToken;
+						fsa.state = 101;
+			
 					}
 					fsa.f--;
-					fsa.state = 0;
 					return;
 				}
 				break;
 			case 13:
 				if(isdigit(c)) {
-					lengthError = 1;
+					realLengthError = 1;
 				}
 				else {
-					if(lengthError) {
-						puts("real machine got length error");
+					if(intLengthError) {
+						struct token errToken;
+						errToken.tokenName = LEXERR;
+						union Attribute attr;
+						attr.attrInt = INTTOOLONG;
+						errToken.attribute = &attr;
+						fsa.currToken = &errToken;
+						fsa.state = 101;
+					
+					}
+					else if(decimalLengthError) {
+						struct token errToken;
+						errToken.tokenName = LEXERR;
+						union Attribute attr;
+						attr.attrInt = DECIMALTOOLONG;
+						errToken.attribute = &attr;
+						fsa.currToken = &errToken;
+						fsa.state = 101;
+				
+					}
+					else if(realLengthError) {
+						struct token errToken;
+						errToken.tokenName = LEXERR;
+						union Attribute attr;
+						attr.attrInt = REALTOOLONG;
+						errToken.attribute = &attr;
+						fsa.currToken = &errToken;
+						fsa.state = 101;
 					}
 					else {
-						printf("real machine got real %s.%se%s\n",d,yy,zz);
+						struct token numToken;
+						numToken.tokenName = NUM;
+						union Attribute attr;
+						strcpy(num,d);
+						strcat(num,".");
+						strcat(num,yy);
+						strcat(num,"E");
+						strcat(num,zz);
+						attr.attrString = num;
+						numToken.attribute = &attr;
+						fsa.currToken = &numToken;
+						fsa.state = 101;
+
 					}
 					fsa.f--;
-					fsa.state = 0;
 					return;
 				}
 				break;
@@ -489,8 +652,13 @@ void realMachine(char *line) {
 					fsa.state = 12;
 				}
 				else {
-					printf("real machine got error5 %c\n", c);
-					fsa.state = 100;
+					struct token errToken;
+					errToken.tokenName = LEXERR;
+					union Attribute attr;
+					attr.attrInt = UNRECOGNIZEDNUM;
+					errToken.attribute = &attr;
+					fsa.currToken = &errToken;
+					fsa.state = 101;
 					fsa.f--;
 					return;
 				}
@@ -501,6 +669,7 @@ void realMachine(char *line) {
 
 void newLineMachine(char* line) {
 	char c = line[fsa.f];
+	// printf("newLine got '%c'\n",c);
 	fsa.f++;
 	if(c == '\n') {
 		puts("found new line");
@@ -515,15 +684,26 @@ void newLineMachine(char* line) {
 
 void addopMachine(char* line) {
 	char c = line[fsa.f];
+	printf("addop got '%c'\n",c);
 	fsa.f++;
 	if(c == '+') {
-		puts("found +");
-		fsa.state = 0;
+		struct token addopToken;
+		addopToken.tokenName = ADDOP;
+		union Attribute attr;
+		attr.attrInt = ADD;
+		addopToken.attribute = &attr;
+		fsa.currToken = &addopToken;
+		fsa.state = 101;
 		return;
 	}
 	else if(c == '-') {
-		puts("found -");
-		fsa.state = 0;
+		struct token addopToken;
+		addopToken.tokenName = ADDOP;
+		union Attribute attr;
+		attr.attrInt = SUBTRACT;
+		addopToken.attribute = &attr;
+		fsa.currToken = &addopToken;
+		fsa.state = 101;
 		return;
 	}
 	//'or' is also an addop but it is a reserved word also
@@ -538,6 +718,7 @@ void assignopMachine(char* line) {
 	while(fsa.f < MAX_LINE_LENGTH && count < 2) {
 		count++;
 		char c = line[fsa.f];
+		// printf("assignop got '%c'\n",c);
 		fsa.f++;
 		if(count == 1 && c != ':') {
 			//first char has to be ':' if not, exit
@@ -553,8 +734,10 @@ void assignopMachine(char* line) {
 		}
 		else if(count == 2 && c == '=') {
 			//if second char is '=', return token
-			fsa.state = 0;
-			puts("found assignop");
+			fsa.state = 101;
+			struct token assignopToken;
+			assignopToken.tokenName = ASSIGNOP;
+			fsa.currToken = &assignopToken;
 			return;
 		}
 	}
@@ -562,15 +745,26 @@ void assignopMachine(char* line) {
 
 void mulopMachine(char *line) {
 	char c = line[fsa.f];
+	// printf("mulop got '%c'\n",c);
 	fsa.f++;
 	if(c == '*') {
-		puts("found *");
-		fsa.state = 0;
+		struct token mulopToken;
+		mulopToken.tokenName = MULOP;
+		union Attribute attr;
+		attr.attrInt = MULTIPLY;
+		mulopToken.attribute = &attr;
+		fsa.currToken = &mulopToken;
+		fsa.state = 101;
 		return;
 	}
 	else if(c == '/') {
-		puts("found /");
-		fsa.state = 0;
+		struct token mulopToken;
+		mulopToken.tokenName = MULOP;
+		union Attribute attr;
+		attr.attrInt = DIVIDE;
+		mulopToken.attribute = &attr;
+		fsa.currToken = &mulopToken;
+		fsa.state = 101;
 		return;
 	}
 	//'div', 'mod', and 'and' are also mulop but are reserved words also
